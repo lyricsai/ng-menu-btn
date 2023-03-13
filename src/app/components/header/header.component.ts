a73234c22817de7c06ba2568edd0135f490d22db
Anastasiya Litvinenka <loffy@rambler.ru>
2023-03-13 11:59:16 +0300

    basic menu burger ng

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,78 +3,85 @@ import { MenuService } from "src/app/services/menu.service";
 import { ActivatedRoute, Event, NavigationEnd, Router } from "@angular/router";
 
 @Component({
-    selector: "app-header",
-    templateUrl: "./header.component.html",
-    styleUrls: ["./header.component.scss"],
-    host: {
-        "(document:click)": "onClick($event)",
-    },
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+  host: {
+    "(document:click)": "onClick($event)",
+  },
 })
 export class HeaderComponent implements OnInit {
-    isShowNav$!: boolean;
-    isMobile$!: boolean;
-    prevRoute: string = window.location.pathname;
-    currRoute: string = window.location.pathname;
+  isShowNav$!: boolean;
+  isMobile$!: boolean;
+  prevRoute: string = window.location.pathname;
+  currRoute: string = window.location.pathname;
 
-    constructor(
-        private router: Router,
-        private menuService: MenuService,
-        private route: ActivatedRoute,
-        private _eref: ElementRef
-    ) {}
+  constructor(
+    private router: Router,
+    private menuService: MenuService,
+    private route: ActivatedRoute,
+    private _eref: ElementRef
+  ) {}
 
-    ngOnInit(): void {
-        this.checkIsMobile();
-        this.checkIsShowNav();
-        this.checkRouteChange();
-    }
+  ngOnInit(): void {
+    this.checkIsMobile();
+    this.checkIsShowNav();
+    this.checkRouteChange();
+  }
 
-    checkIsShowNav() {
-        this.menuService.checkIsShowNav.subscribe((data: any) => {
-            this.isShowNav$ = data;
-        });
-    }
+  checkIsShowNav() {
+    this.menuService.checkIsShowNav.subscribe((data: any) => {
+      this.isShowNav$ = data;
+    });
+  }
 
-    toggleIsShowNav() {
-        this.menuService.toggleIsShowNav();
-        this.checkIsShowNav();
-    }
+  toggleIsShowNav() {
+    this.menuService.toggleIsShowNav();
+    this.checkIsShowNav();
+  }
 
-    checkIsMobile() {
-        this.menuService.checkIsMobile.subscribe((data: any) => {
-            this.isMobile$ = data;
-        });
-    }
+  checkIsMobile() {
+    this.menuService.checkIsMobile.subscribe((data: any) => {
+      this.isMobile$ = data;
+    });
+  }
 
-    checkRouteChange() {
-        this.checkRoute();
-        this.checkIsMobile();
-    }
+  checkRouteChange() {
+    this.checkRoute();
+    this.checkIsMobile();
+  }
 
-    onResize() {
-        this.checkIsMobile();
-        this.checkIsShowNav();
-    }
+  onResize() {
+    this.checkIsMobile();
+    this.checkIsShowNav();
+  }
 
-    checkRoute() {
-        this.router.events.subscribe((e: Event) => {
-            if (e instanceof NavigationEnd) {
-                if (this.isMobile$ && e.url !== this.currRoute) {
-                    this.prevRoute = this.currRoute;
-                    this.currRoute = e.url;
-                    this.toggleIsShowNav();
-                }
-            }
-        });
-    }
+  checkRoute() {
+    this.router.events.subscribe((e: Event) => {
+      if (e instanceof NavigationEnd) {
+        if (this.isMobile$ && e.url !== this.currRoute) {
+          this.prevRoute = this.currRoute;
+          this.currRoute = e.url;
+          this.toggleIsShowNav();
+        }
+      }
+    });
+  }
 
-    onClick(event: any) {
-        if (
-            this.isMobile$ &&
-            this.isShowNav$ &&
-            !this._eref.nativeElement.contains(event.target)
-        )
-            // or some similar check
-            this.toggleIsShowNav();
-    }
+  onClick(event: any) {
+    console.log(
+      this._eref.nativeElement.children[0].children[2]?.children[0]
+        ?.children[0],
+      event.target
+    );
+    if (
+      this.isMobile$ &&
+      this.isShowNav$ &&
+      (!this._eref.nativeElement.contains(event.target) ||
+        this._eref.nativeElement.children[0].children[2]?.children[0]
+          ?.children[0] === event.target)
+    )
+      // or some similar check
+      this.toggleIsShowNav();
+  }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,14 +1,13 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { MenuService } from "src/app/services/menu.service";
-import { Observable } from "rxjs";
 
 @Component({
-    selector: "app-nav",
-    templateUrl: "./nav.component.html",
-    styleUrls: ["./nav.component.scss"],
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.scss"],
 })
 export class NavComponent implements OnInit {
-    constructor() {}
+  constructor() {}
+  @Input() isShowNav!: boolean;
 
-    ngOnInit(): void {}
+  ngOnInit(): void {}
 }
